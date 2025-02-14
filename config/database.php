ff64ffa941cd78ac8323ba1f72c5bf6bb8adcee2
tejaspanchall <tejaspanchal127@gmail.com>
2025-02-14 15:04:43 +0530

    changes variables

--- config/database.php
+++ config/database.php
@@ -5,11 +5,11 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
 $db_config = [
-    'host' => getenv('DB_HOST'),
-    'port' => getenv('DB_PORT'),
-    'dbname' => getenv('DB_NAME'),
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD')
+    'host' => getenv('PGHOST'),
+    'port' => getenv('PGPORT'),
+    'dbname' => getenv('PGDATABASE'),
+    'user' => getenv('PGUSER'),
+    'password' => getenv('PGPASSWORD')
 ];
 
 try {
