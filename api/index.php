62763e90af0a4adf6f5c70033e9bc5322345f1e4
tejaspanchall <tejaspanchal127@gmail.com>
2025-02-14 15:24:37 +0530

    ../

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-require_once "config/database.php";
+require_once "../config/database.php";
 $query = $pdo->query("SELECT * FROM test");
 $users = $query->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode(["status" => "success", "data" => $users]);
